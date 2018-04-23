dad5a235c1099d1eb10046fda8bc3d3bab66059b
izakharov <admin@bukkit-minecraft.ru>
2018-04-23 16:36:13 +0300

    Message Push

--- Rune.java
+++ Rune.java
@@ -2,7 +2,7 @@ package ru.foxit.grayfox;
 
 public class Rune {
     public static void main(String[] args) {
-        System.out.println("not fix"); // hello?
+        System.out.println("not fix"); // Ты научился пользоваться GIT?
     }
 }
 
